e1b03e66bec660a41e627c798df68807fff103c8
Jeyakumar <jeyakumar1442@gmail.com>
2024-09-13 09:43:15 +0530

    files added

---
+++ FORMS/script.js
@@ -0,0 +1,8 @@
+function disdetials() {
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const gender = document.getElementById("gender").value;
+
+    alert(`Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nGender: ${gender}`);
+}
